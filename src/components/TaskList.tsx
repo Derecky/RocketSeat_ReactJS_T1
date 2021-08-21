318381822e26544ea783a49096abeda73732076a
Derecky Costa <dereckycosta@gmail.com>
2021-08-21 13:51:27 -0300

    completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,50 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [count, setCount] = useState(0);
+
+  var ID = function () {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    
+    const newTask: Task = {
+      id: count,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+
+    setTasks(old => [...old, newTask])
+    setCount(count + 1)
+   
+
+    setNewTaskTitle('')
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task ))
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task) => 
+      task.id!==id
+    ))
   }
 
   return (
